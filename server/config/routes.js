49906487848db5b98e7e002230e2c80554192097
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-12 14:42:36 +0100

    added additional routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,6 +2,23 @@
 module.exports = (app,config) =>{
     app.get("/", function (req,res) {
         res.sendFile(config.rootPath+"\public\\"+"\index.html");
-    })
+    });
+
+    app.get("/games.html", function (req,res) {
+        res.sendFile(config.rootPath+"\public\\"+"\games.html");
+    });
+
+    app.get("/quiet.html", function (req,res) {
+        res.sendFile(config.rootPath+"\public\\"+"\quiet.html");
+    });
+
+    app.get("/meet.html", function (req,res) {
+        res.sendFile(config.rootPath+"\public\\"+"\meet.html");
+    });
+
+    app.get("/work.html", function (req,res) {
+        res.sendFile(config.rootPath+"\public\\"+"\work.html");
+    });t 
+
 
 };
